66af2025143e91ecc433c83d8df2c29596a33065
Barry <barrybrands02@hotmail.com>
2020-03-16 18:39:13 +0100

    Abstract maken templates

--- api/src/Controller/PitchController.php
+++ api/src/Controller/PitchController.php
@@ -24,18 +24,40 @@ class PitchController extends AbstractController
      * @Route("/")
      * @Template
      */
-	public function indexAction()
+    public function indexAction()
     {
-    	return [];
+        $pitches = [];
+
+        for ($i = 1; $i < 4; $i++) {
+
+            $pitch = [];
+            $pitch["id"] = $i;
+            $pitch['name'] = "Een betere wereld voor iedereen";
+            $pitch['description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu faucibus odio, nec
+                                molestie orci. Phasellus sollicitudin cursus ullamcorper.  Nunc quis sapien non felis
+                                 bibendum tempor et id tortor. Mauris felis sapien, condimentum ullamcorper tincidunt 
+                                 sit amet, commodo sed ex. Nam malesuada quis neque sit amet imperdiet. Ut nec est 
+                                 pharetra leo varius varius ac id orci. Nulla euismod vestibulum eros eu sagittis.";
+            $pitches[] = $pitch;
+        }
+
+        return ["pitches" => $pitches];
     }
 
     /**
      * @Route("/{id}")
      * @Template
      */
-	public function viewAction($id)
+    public function viewAction($id)
     {
-    	return [];
+        $pitch = [];
+        $pitch['name'] = "Een betere wereld voor iedereen";
+        $pitch['description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu faucibus odio, nec
+                                molestie orci. Phasellus sollicitudin cursus ullamcorper.  Nunc quis sapien non felis
+                                 bibendum tempor et id tortor. Mauris felis sapien, condimentum ullamcorper tincidunt 
+                                 sit amet, commodo sed ex. Nam malesuada quis neque sit amet imperdiet. Ut nec est 
+                                 pharetra leo varius varius ac id orci. Nulla euismod vestibulum eros eu sagittis.";
+        return ["pitch" => $pitch];
     }
 }
 
