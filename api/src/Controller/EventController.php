c68c72c8990619364f5a62a5a75fdfb0cbef1003
Barry <barrybrands02@hotmail.com>
2020-03-16 14:29:34 +0100

    List items widget added

--- api/src/Controller/EventController.php
+++ api/src/Controller/EventController.php
@@ -24,21 +24,41 @@ class EventController extends AbstractController
      * @Route("/")
      * @Template
      */
-	public function indexAction()
-    {	
-    	return [];
+    public function indexAction()
+    {
+        $events = [];
+        
+        $event = [];
+        $event["id"] = 1;
+        $event['name'] = "Event";
+        $event['description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu faucibus odio, nec
+                                molestie orci. Phasellus sollicitudin cursus ullamcorper. Nunc quis sapien non felis bibendum
+                                tempor et id tortor. Mauris felis sapien, condimentum ullamcorper tincidunt sit amet, commodo
+                                sed ex. Nam malesuada quis neque sit amet imperdiet. Ut nec est pharetra leo varius varius ac id
+                                orci. Nulla euismod vestibulum eros eu sagittis.";
+        $events[] = $event;
+
+        $event = [];
+        $event["id"] = 2;
+        $event['name'] = "Event";
+        $event['description'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu faucibus odio, nec
+                                molestie orci. Phasellus sollicitudin cursus ullamcorper. Nunc quis sapien non felis bibendum
+                                tempor et id tortor. Mauris felis sapien, condimentum ullamcorper tincidunt sit amet, commodo
+                                sed ex. Nam malesuada quis neque sit amet imperdiet. Ut nec est pharetra leo varius varius ac id
+                                orci. Nulla euismod vestibulum eros eu sagittis.";
+        $events[] = $event;
+
+    	return ["events"=>$events];
     }
 
     /**
      * @Route("/{id}")
      * @Template
      */
-	public function viewAction($id)
+    public function viewAction($id)
     {
-    	return [];
+        return [];
     }
-
-
 
 
 }
